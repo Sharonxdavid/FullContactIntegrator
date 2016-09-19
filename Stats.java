22366e31530850bd1c7c86ab7703b54d1e0e07d3
Sharon D <sharonxdavid@gmail.com>
2016-09-20 00:36:54 +0300

    added stats

---
+++ Stats.java
@@ -0,0 +1,34 @@
+
+public class Stats {
+	
+	private int femaleCounter;
+	private int maleCounter;
+	private double femaleAccuracyAvg;
+	private double maleAccuracyAvg;
+	public int getFemaleCounter() {
+		return femaleCounter;
+	}
+	public void setFemaleCounter(int femaleCounter) {
+		this.femaleCounter = femaleCounter;
+	}
+	public int getMaleCounter() {
+		return maleCounter;
+	}
+	public void setMaleCounter(int maleCounter) {
+		this.maleCounter = maleCounter;
+	}
+	public double getFemaleAccuracyAvg() {
+		return femaleAccuracyAvg;
+	}
+	public void setFemaleAccuracyAvg(double femaleAccuracyAvg) {
+		this.femaleAccuracyAvg = femaleAccuracyAvg;
+	}
+	public double getMaleAccuracyAvg() {
+		return maleAccuracyAvg;
+	}
+	public void setMaleAccuracyAvg(double maleAccuracyAvg) {
+		this.maleAccuracyAvg = maleAccuracyAvg;
+	}
+	
+
+}
