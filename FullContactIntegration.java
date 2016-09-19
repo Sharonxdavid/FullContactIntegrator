5bb9894183d8bcb15aa88e8536fba0357219af0a
Sharon D <sharonxdavid@gmail.com>
2016-09-19 23:23:14 +0300

    added statistics

--- FullContactIntegration.java
+++ FullContactIntegration.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -14,6 +15,8 @@ public class FullContactIntegration {
 
 	public static String[] emailAddress;
 	public static String apiKey = "68e63d17dd554d79";
+	public static Stats stats = new Stats();
+	public static HashMap<String, String> dictionary;
 	
 	//read input file and store in a stringbuilder
 	public static StringBuilder readFromInputFile() throws IOException{
@@ -34,7 +37,13 @@ public class FullContactIntegration {
 		emailAddress = sb.toString().split("\r\n");
 	}
 	
-	//send http reqest and receive specific contact information
+	/**
+	 * send http reqest and receive specific contact information
+	 * @param name
+	 * @return
+	 * @throws UnsupportedEncodingException
+	 * @throws IOException
+	 */
 	public static String getContactDetails(String name) throws UnsupportedEncodingException, IOException{
 		URL url = new URL("https://api.fullcontact.com/v2/person.json?email=" + name);
 		
@@ -61,7 +70,7 @@ public class FullContactIntegration {
 		in.close();
 
 		//print result
-		System.out.println(response.toString());
+		//System.out.println(response.toString());
 		return (response.toString());
 		
 	}
@@ -71,7 +80,7 @@ public class FullContactIntegration {
 	public static void parseResponse(String jsonAsString){
 		int age = 0;
 		JsonObject jsonObject = new JsonParser().parse(jsonAsString).getAsJsonObject();
-		String likeli = "";
+		double likeli = 0;
 		String fullName = "";
 		//age = jsonObject.get("demographics").getAsJsonObject().get("age").getAsInt();
 		String gender = "";
@@ -79,7 +88,7 @@ public class FullContactIntegration {
 		String country = "";
 		
 		try{
-		 likeli = jsonObject.get("likelihood").getAsString();
+		 likeli = jsonObject.get("likelihood").getAsDouble();
 		 fullName = jsonObject.get("contactInfo").getAsJsonObject().get("fullName").getAsString();
 		//age = jsonObject.get("demographics").getAsJsonObject().get("age").getAsInt();
 		 gender = jsonObject.get("demographics").getAsJsonObject().get("gender").getAsString();
@@ -89,9 +98,6 @@ public class FullContactIntegration {
 			System.out.println("not found");
 		}
 		
-//		if(!(likeli == null)){
-//			System.out.print(likeli + ", ");
-//		}
 		if(!(fullName == null)){
 			System.out.print(fullName+ ", ");
 		}
@@ -100,6 +106,15 @@ public class FullContactIntegration {
 		}
 		if (!(gender == null)){
 			System.out.print(gender+ ", ");
+			if(gender.equalsIgnoreCase("female")){
+				stats.setFemaleCounter(stats.getFemaleCounter() + 1);
+				stats.setFemaleAccuracyAvg((stats.getFemaleAccuracyAvg() + likeli) / stats.getFemaleCounter());
+			}
+			if(gender.equalsIgnoreCase("male")){
+				stats.setFemaleCounter(stats.getMaleCounter() + 1);
+				stats.setMaleAccuracyAvg((stats.getMaleAccuracyAvg() + likeli) / stats.getMaleCounter());
+			}
+			
 		}
 		if (!(city == null)){
 			System.out.print(city + " ");
@@ -108,17 +123,10 @@ public class FullContactIntegration {
 			System.out.println(country);
 		}
 		
-		
-		//get name, age, gender, company, address(city, street and number, country)
-//		System.out.println(jsonObject.get("contactInfo").getAsString());
-//		System.out.println(jsonObject.get("status").getAsString());
-//		System.out.println(jsonObject.get("status").getAsString());
-//		System.out.println(jsonObject.get("status").getAsString());
-//		System.out.println(jsonObject.get("status").getAsString());
-		
 	}
 	
 	public static void main(String[] args) throws IOException {
+		dictionary = new HashMap<>();
 		
 		parseInputFile(readFromInputFile());
 		String jsonRes;
@@ -131,6 +139,9 @@ public class FullContactIntegration {
 		
 		System.out.println("Here is the full list:");
 		for (int i = 0; i < emailAddress.length; i++) {
+			if(dictionary.containsKey(emailAddress[i])){
+				System.out.println(dictionary.get(emailAddress[i]));
+			}
 			System.out.println(emailAddress[i]);
 //			jsonRes = getContactDetails("bart@fullcontact.com");
 			jsonRes = getContactDetails(emailAddress[i]);
